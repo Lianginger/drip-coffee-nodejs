65baf3274d05ac18604e60381295bb4346147d83
lianginger <jackyliang444@hotmail.com.tw>
2020-03-28 23:10:12 +0800

    feature(app.js): add handlebar helper to parse json

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ const cors = require('cors')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.engine('handlebars', exphbs())
+app.engine(
+  'handlebars',
+  exphbs({
+    helpers: {
+      json: function(context) {
+        return JSON.stringify(context)
+      }
+    }
+  })
+)
 app.set('view engine', 'handlebars')
 
 app.use('/', require('./routes/index'))
